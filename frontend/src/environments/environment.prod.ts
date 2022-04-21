459f7551a0ff273150522659941b5479eb5dab27
SayPlease <saypleaze@outlook.com>
2022-04-21 18:04:32 +0300

    implement firebase

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angularmessenger-87e06',
+    appId: '1:532413619498:web:ecd32f2b893706cd85d253',
+    storageBucket: 'angularmessenger-87e06.appspot.com',
+    apiKey: 'AIzaSyB7SxA0F_6Z27k52j4cVvxJ8hV-qAAzhbY',
+    authDomain: 'angularmessenger-87e06.firebaseapp.com',
+    messagingSenderId: '532413619498',
+    measurementId: 'G-YNW3H8WCJ1',
+  },
   production: true
 };
